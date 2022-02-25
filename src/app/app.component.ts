dd86abf00ea4786554d9a9ee49d6b41650274334
Harvy <hw_haowei@hotmail.com>
2022-02-25 18:41:58 +0800

    Final Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   pendingList = [];
   successList = [];
   cookingList = [];
   bot: number = 3;
   id: number = 1;
   vid: number = 1;
-
+  sec: number = 10
+  interval: any
+  
   ngOnInit() { 
-    
+    // this.interval = setInterval(() => {
+    //   for(let i = 0; i < this.cookingList.length; i++) this.cookingList[i].i--
+    // }, 1000)
+  }
+
+  ngOnDestroy(): void {
+      clearInterval(this.interval)
   }
 
   counter(i: number) {
@@ -39,20 +47,25 @@ export class AppComponent implements OnInit {
 
   onCooking() {
     if (this.cookingList.length < this.bot && this.pendingList.length != 0) {
-
-      let timeout = setTimeout(() => {
+        let timeout = setTimeout(() => {
         // Move cooking to success
         this.successList.push(this.cookingList[0]);
+        // Stop the index Timeout
         clearTimeout(this.cookingList[0].timeout);
+        // Drop success
         this.cookingList = this.cookingList.filter(
           (list) => list != this.cookingList[0] && list != undefined
         );
-        // After bot is free, check pending
-        if (this.pendingList.length != 0) this.onCooking();
-      }, 10000);
-
+        // Check pending
+        this.onCooking();
+      }, this.sec * 1000);
       // Move pending to cooking
-      this.cookingList.push({ id: this.pendingList[0], timeout });
+      this.cookingList.push({ 
+        id: this.pendingList[0], 
+        timeout, 
+        // i: this.sec 
+      });
+      // Drop pending
       this.pendingList = this.pendingList.filter(
         (list) => list != this.pendingList[0] && list != undefined
       );
@@ -64,9 +77,12 @@ export class AppComponent implements OnInit {
       this.bot++;
     } else if (type == 'minus') {
       this.bot--;
+      // Drop cooking if over bot
       if (this.cookingList.length > this.bot) {
+        // Add cooking to pending
         this.pendingList.unshift(this.cookingList[this.cookingList.length - 1].id);
         clearTimeout(this.cookingList[this.cookingList.length - 1].timeout)
+        // Drop last element in cooking
         this.cookingList.pop();
       }
     }
